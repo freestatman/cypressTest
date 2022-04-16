2c2f256e2f5b6560e1124ba8061d66cbb2755e1b
Freeman Wang <shuaichengwang@gmail.com>
2022-04-15 20:49:42 -0400

    test more cypress and rhino commands

--- tests/cypress/integration/app.spec.js
+++ tests/cypress/integration/app.spec.js
@@ -1,7 +1,20 @@
-describe('app', () => {
+describe('shiny app', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
   it('starts', () => {})
 })
+
+/// <reference types="cypress" /> 
+
+describe('My First Test', () => {
+    it('test google.com', () => {
+
+        cy.visit("www.google.com")
+        cy.get(".gLFyf").type("R Shiny development with nodejs{enter}")
+        cy.contains("Videos").click()
+
+    })
+})
+
